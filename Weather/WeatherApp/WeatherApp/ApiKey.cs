5f5a6e22d60f483625ff4c04c5e31e6d4511c2dc
Dave Ortinau <Dave Ortinau>
2017-05-11 10:23:12 -0700

    key

---
+++ Weather/WeatherApp/WeatherApp/ApiKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class Api
+    {
+        internal static readonly string Key = "bb870ad9260d463680ecf5ec1f5a05ea";
+    }
+}
